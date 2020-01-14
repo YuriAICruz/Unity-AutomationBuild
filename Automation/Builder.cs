812283652eb4e176bcd26ae4e96c817755ce825a
YuriAICruz <yuriaicruz@gmail.com>
2020-01-14 16:26:19 -0300

    simple android build, package

---
+++ Automation/Builder.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+public class Builder : MonoBehaviour
+{
+    [MenuItem("Automation/Build")]
+    static void PerformBuild()
+    {
+        string[] _scenes = EditorBuildSettings.scenes.Select(x => x.path).ToArray();
+        BuildPipeline.BuildPlayer(_scenes, "Builds/test.apk", BuildTarget.Android, BuildOptions.None);
+    }
+}
