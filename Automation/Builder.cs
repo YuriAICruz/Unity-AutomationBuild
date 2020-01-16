ff2d36f7348d971148abcc0ac27dadcb240b8eea
YuriAICruz <yuriaicruz@gmail.com>
2020-01-16 14:35:36 -0300

    add ios build

--- Automation/Builder.cs
+++ Automation/Builder.cs
@@ -6,10 +6,17 @@ using UnityEngine;
 
 public class Builder : MonoBehaviour
 {
-    [MenuItem("Automation/Build")]
-    static void PerformBuild()
+    [MenuItem("Automation/Android/Build")]
+    static void PerformBuildAndroid()
     {
         string[] _scenes = EditorBuildSettings.scenes.Select(x => x.path).ToArray();
         BuildPipeline.BuildPlayer(_scenes, "Builds/test.apk", BuildTarget.Android, BuildOptions.None);
+    }
+
+    [MenuItem("Automation/iOS/Build")]
+    static void PerformBuildiOS()
+    {
+        string[] _scenes = EditorBuildSettings.scenes.Select(x => x.path).ToArray();
+        BuildPipeline.BuildPlayer(_scenes, "Builds/iOS", BuildTarget.iOS, BuildOptions.None);
     }
 }
