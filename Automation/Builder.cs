e08226379bfdbc459752b29dffb0a04a4cbda517
Yuri <yuriaicruz@gmail.com>
2020-01-18 18:54:26 -0300

    builder

--- Automation/Builder.cs
+++ Automation/Builder.cs
@@ -1,22 +1,55 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using UnityEditor;
 using UnityEngine;
 
-public class Builder : MonoBehaviour
+namespace Graphene.Automation
 {
-    [MenuItem("Automation/Android/Build")]
-    static void PerformBuildAndroid()
+    public class Builder : MonoBehaviour
     {
-        string[] _scenes = EditorBuildSettings.scenes.Select(x => x.path).ToArray();
-        BuildPipeline.BuildPlayer(_scenes, "Builds/test.apk", BuildTarget.Android, BuildOptions.None);
-    }
+        private static string[] _args;
+         
+        private static string GetCommandLine(string key)
+        {
+            if (_args == null)
+            {
+                _args = System.Environment.GetCommandLineArgs();
+            }
+            
+            for (int i = 0; i < _args.Length; i++)
+            {
+                Debug.Log("ARG " + i + ": " + _args[i]);
+                
+                if (key == _args[i])
+                {
+                    return _args[i + 1];
+                }
+            }
+            
+            return "";
+        }
+        
+        
+        [MenuItem("Automation/Android/Build")]
+        public static void PerformBuildAndroid()
+        {
+            var scenes = EditorBuildSettings.scenes.Select(x => x.path).ToArray();
+            BuildPipeline.BuildPlayer(scenes, "Builds/Android/appVersion.apk", BuildTarget.Android, BuildOptions.None);
+        }
+    
+        [MenuItem("Automation/Oculus/Quest/Build")]
+        public static void PerformBuildOculusQuest()
+        {
+            GetCommandLine("fake");
+            
+            var scenes = EditorBuildSettings.scenes.Select(x => x.path).ToArray();
+            BuildPipeline.BuildPlayer(scenes, "Builds/Oculus/questApp.apk", BuildTarget.Android, BuildOptions.None);
+        }
 
-    [MenuItem("Automation/iOS/Build")]
-    static void PerformBuildiOS()
-    {
-        string[] _scenes = EditorBuildSettings.scenes.Select(x => x.path).ToArray();
-        BuildPipeline.BuildPlayer(_scenes, "Builds/iOS", BuildTarget.iOS, BuildOptions.None);
+        [MenuItem("Automation/iOS/Build")]
+        public static void PerformBuildiOs()
+        {
+            var scenes = EditorBuildSettings.scenes.Select(x => x.path).ToArray();
+            BuildPipeline.BuildPlayer(scenes, "Builds/iOS", BuildTarget.iOS, BuildOptions.None);
+        }
     }
 }
